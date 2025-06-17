ca66471cd4ebfaa82626b67b6b8b3f50f207b50e
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-17 23:40:57 +0530

    Added for return string

--- main/main.go
+++ main/main.go
@@ -6,8 +6,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func returnString(a, b string) (string, string) {
+	return b, a
+}
+
 func main() {
-	ans := add(4, 9)
+	ans, ans1 := returnString("Adi", "Lucky")
 	fmt.Println(ans)
+	fmt.Println(ans1)
 
 }
