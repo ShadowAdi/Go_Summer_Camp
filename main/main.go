7ec098565db6c921919ed714d60ea1a2ce33edb7
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-18 00:07:45 +0530

    Checking defer

--- main/main.go
+++ main/main.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	sum := 19
-	if sum < 5 {
-		fmt.Println("The Value of the sum is: ", sum)
-	} else {
-		fmt.Println("The sum should be: ", sum)
-	}
+	defer fmt.Println("World")
+	fmt.Println("Hello")
 }
