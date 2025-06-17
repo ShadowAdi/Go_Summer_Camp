0dfdf240d13242aaa6241206d6a43ad083f5993d
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-18 00:18:30 +0530

    checking array

--- main/main.go
+++ main/main.go
@@ -1,10 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	defer fmt.Println("World")
-	fmt.Println("Hello")
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println("a is ", a[0])
+	fmt.Println("a[1] is: ", a[1])
+
 }
