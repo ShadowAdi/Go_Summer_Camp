83d1a5a647437ecfffa065d380c8c3dfbef3b5cc
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-17 23:46:49 +0530

    Check for short variable

--- main/main.go
+++ main/main.go
@@ -2,18 +2,14 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
-}
-
-func returnString(a, b string) (string, string) {
-	return b, a
-}
+var num1, num2 int = 1, 2
 
 func main() {
-	var ans, ans1 string
-	ans, ans1 = returnString("Adi", "Lucky")
-	fmt.Println(ans)
-	fmt.Println(ans1)
+	a, b, c := true, false, "Hii"
+	fmt.Println("The value of num1 is: ", num1)
+	fmt.Println("The value of num2 is: ", num2)
+	fmt.Println("The value of a is: ", a)
+	fmt.Println("The value of b is: ", b)
+	fmt.Println("The value of c is: ", c)
 
 }
