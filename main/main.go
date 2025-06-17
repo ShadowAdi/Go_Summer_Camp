fffbf8b7eb5d004b7a4982235f30dc0c78537cd7
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-17 23:52:22 +0530

    Added Initial Types

--- main/main.go
+++ main/main.go
@@ -2,14 +2,9 @@ package main
 
 import "fmt"
 
-var num1, num2 int = 1, 2
-
 func main() {
-	a, b, c := true, false, "Hii"
-	fmt.Println("The value of num1 is: ", num1)
-	fmt.Println("The value of num2 is: ", num2)
-	fmt.Println("The value of a is: ", a)
-	fmt.Println("The value of b is: ", b)
-	fmt.Println("The value of c is: ", c)
-
+	i := 12
+	f := float64(i)
+	u := uint(f)
+	fmt.Println(i, f, u)
 }
