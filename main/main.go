9499d4790845f294b98a3190fed7a77e42dfa029
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-18 00:28:46 +0530

    Make in go

--- main/main.go
+++ main/main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println("a is ", a[0])
-	fmt.Println("a[1] is: ", a[1])
+	s := [6]int{1, 2, 3, 4, 5, 7}
+	fmt.Println(s)
+	a := make([]int, 2)
+	a = append(a, 9)
+	fmt.Println(a)
 
 }
