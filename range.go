1a305bdd527bd4e138d45758381b3fc02cd6a19a
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 21:39:25 +0530

    Add reverse

--- range.go
+++ range.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
-	var arr1 = [3]int{10, 11, 1}
-	var maxNum int
-	for _, v := range arr1 {
-		maxNum = max(maxNum, v)
+func reverse(slice []int) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
 	}
-	fmt.Println("The value is %d: ", maxNum)
+}
 
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println("Before:", arr)
+	reverse(arr)
+	fmt.Println("After:", arr)
 }
