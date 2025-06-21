8f84e95809e1a1dd4da19663784ef1c0ad449157
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 22:00:10 +0530

    add character frequency

--- range.go
+++ range.go
@@ -2,22 +2,14 @@ package main
 
 import "fmt"
 
-func reverse(slice []int) {
-	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
-		slice[i], slice[j] = slice[j], slice[i]
-	}
-}
-
 func main() {
-	arr := make([]int, 5)
-	for i, _ := range arr {
-		arr[i] = i + 1
+	ans := "aditya"
+	mpp := make(map[rune]int)
+	for _, ch := range ans {
+		mpp[ch]++
 	}
 
-	arr = append(arr, 4)
-
-	moreNumbers := []int{8, 9, 10}
-	arr = append(arr, moreNumbers...)
-	fmt.Println("After appending another slice:", arr)
-
+	for k, v := range mpp {
+		fmt.Printf("%q: %d\n", k, v)
+	}
 }
