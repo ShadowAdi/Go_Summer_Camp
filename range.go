1150c0191d88433f8ce6205944f2dcf87255d0e5
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 19:33:48 +0530

    added fibonacci

--- range.go
+++ range.go
@@ -9,7 +9,19 @@ func factorial(n int64) int64 {
 	return n * factorial(n-1)
 }
 
+func fibonaaci() {
+	n := 10
+	a, b := 0, 1
+	fmt.Println("Fibonacci Series:")
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", a)
+		next := a + b
+		a = b
+		b = next
+	}
+	fmt.Println()
+}
+
 func main() {
-	result := factorial(5)
-	fmt.Println("Factorial of 5 is:", result)
+	fibonaaci()
 }
