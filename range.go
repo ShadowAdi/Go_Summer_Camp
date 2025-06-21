13ee4466972aa96494ceed0d02101108fa3a9c0e
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 22:05:39 +0530

    Added anargam

--- range.go
+++ range.go
@@ -4,12 +4,35 @@ import "fmt"
 
 func main() {
 	ans := "aditya"
-	mpp := make(map[rune]int)
-	for _, ch := range ans {
-		mpp[ch]++
+	ans1 := "dityaa"
+
+	if isAnagram(ans, ans1) {
+		fmt.Println("They are equal")
+	} else {
+		fmt.Println("They are not equal")
+	}
+}
+
+func isAnagram(a, b string) bool {
+	if len(a) != len(b) {
+		return false
 	}
 
-	for k, v := range mpp {
-		fmt.Printf("%q: %d\n", k, v)
+	m1 := make(map[rune]int)
+	m2 := make(map[rune]int)
+
+	for _, ch := range a {
+		m1[ch]++
 	}
+	for _, ch := range b {
+		m2[ch]++
+	}
+
+	for k, v := range m1 {
+		if m2[k] != v {
+			return false
+		}
+	}
+
+	return true
 }
